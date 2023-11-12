46c55b93c579d67f6ba845d8b8b4fd36cd7b3f59
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-11-11 22:59:29 -0800

    fix: 🐛 fix auth(?), start followup skel

--- services/src/send-followup.ts
+++ services/src/send-followup.ts
@@ -4,5 +4,7 @@ import type { Handler } from "aws-lambda";
 const prisma = new PrismaClient();
 
 export const handler: Handler = async (_event, _context) => {
-
-}
+  (await prisma.contact.findMany({ where: { status: "INITIAL_EMAIL_SENT" } }))
+    .filter((x) => x.followupDate && x.followupDate < new Date())
+    .forEach((_) => {}); // TODO implement email send logic
+};
---
+++ src/routes/(auth)/signin/+page.svelte
@@ -0,0 +1,1 @@
+<a href="/signin/google">Sign in with Google</a>
