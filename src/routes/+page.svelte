3d60d2313360b7b1e65e7c3e5c0dafb15b473c85
Alexander Liu <a@alexanderliu.com>
2023-11-12 22:11:08 -0800

    feat: ✨ contacts table

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -6,29 +6,70 @@
     queryKey: ["repoData"],
     queryFn: async () => await fetch("/api/contacts").then((r) => r.json()),
   });
-
-  $: console.log($query.data);
 </script>
 
-<table>
-  <thead>
-    <tr>
-      <th>Company</th>
-      <th>Contact Name</th>
-      <th>Contact Email</th>
-      <th>Status</th>
-    </tr>
-  </thead>
-  <tbody>
-    {#if $query.isSuccess}
-      {#each $query.data as { id, name, email, company, status } (id)}
-        <tr>
-          <td>{company.name}</td>
-          <td>{name}</td>
-          <td>{email}</td>
-          <td>{status}</td>
-        </tr>
-      {/each}
-    {/if}
-  </tbody>
-</table>
+<main>
+  <table>
+    <thead>
+      <tr>
+        <th>Company</th>
+        <th>Contact Name</th>
+        <th>Contact Email</th>
+        <th>Title</th>
+        <th>Status</th>
+        <th>Last Contact Date</th>
+        <th>Followup Date</th>
+      </tr>
+    </thead>
+    <tbody>
+      {#if $query.isSuccess}
+        {#each $query.data as { id, name, email, title, status, lastContactDate, followupDate, company } (id)}
+          <tr>
+            <td>{company.name}</td>
+            <td>{name}</td>
+            <td>{email ?? ""}</td>
+            <td>{title ?? ""}</td>
+            <td>{status}</td>
+            <td>{lastContactDate ?? ""}</td>
+            <td>{followupDate ?? ""}</td>
+          </tr>
+        {/each}
+      {/if}
+    </tbody>
+  </table>
+</main>
+
+<style lang="scss">
+  main {
+    overflow-x: auto;
+  }
+
+  table {
+    text-align: left;
+    min-width: 100%;
+    border-spacing: 0;
+
+    th,
+    td {
+      padding: 6px 12px 6px 6px;
+      white-space: nowrap;
+      font-size: 14px;
+
+      &:first-child {
+        border-left: none;
+        padding-left: 16px;
+      }
+    }
+
+    th {
+      border-left: 0.75px solid var(--gray100);
+      border-bottom: 1px solid var(--gray100);
+    }
+
+    td {
+      $border: 0.75px solid var(--gray100);
+      border-left: $border;
+      border-bottom: $border;
+    }
+  }
+</style>
