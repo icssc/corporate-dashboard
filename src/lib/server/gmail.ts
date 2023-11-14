0da3be35a103f77f343f6f06cb1a9b9b52901453
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2023-11-14 13:30:59 -0800

    fix: 🐛 use one oauth app for everything

--- src/lib/server/gmail.ts
+++ src/lib/server/gmail.ts
@@ -1,7 +1,11 @@
 import { google } from "googleapis";
 
-import { MAILER_CLIENT_ID, MAILER_CLIENT_SECRET, MAILER_REFRESH_TOKEN } from "$env/static/private";
+import {
+  GOOGLE_OAUTH_CLIENT_ID,
+  GOOGLE_OAUTH_CLIENT_SECRET,
+  MAILER_REFRESH_TOKEN,
+} from "$env/static/private";
 
-const auth = new google.auth.OAuth2(MAILER_CLIENT_ID, MAILER_CLIENT_SECRET);
+const auth = new google.auth.OAuth2(GOOGLE_OAUTH_CLIENT_ID, GOOGLE_OAUTH_CLIENT_SECRET);
 auth.setCredentials({ refresh_token: MAILER_REFRESH_TOKEN });
 export const gmail = google.gmail({ version: "v1", auth });
