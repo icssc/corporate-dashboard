78ee809a2bc3bdb426a064cff95ab79d7fdfe94f
Eddy Chen <89349085+ecxyzzy@users.noreply.github.com>
2024-03-07 11:23:20 -0800

    feat: ✨ implement cronjob and db logic for sending emails (#25)
    * feat: ✨ implement cronjob and db logic for sending emails
    
    * fix: 🐛 email database schema
    
    * fix: 🐛 update mimetext for Mailbox class
    
    ---------
    
    Co-authored-by: Alexander Liu <a@alexanderliu.com>

--- src/lib/db/schema.ts
+++ src/lib/db/schema.ts
@@ -1,5 +1,5 @@
 import { relations } from "drizzle-orm";
-import { pgSchema, pgEnum, text, timestamp, bigint, varchar } from "drizzle-orm/pg-core";
+import { pgSchema, pgEnum, text, timestamp, bigint, varchar, uuid } from "drizzle-orm/pg-core";
 
 export const schema = pgSchema("dev");
 
@@ -98,3 +98,9 @@ export const contactRelations = relations(contact, ({ one }) => ({
     references: [user.id],
   }),
 }));
+
+export const email = schema.table("email", {
+  id: uuid("id").primaryKey().defaultRandom(),
+  submittedAt: timestamp("submitted_at").defaultNow(),
+  raw: text("raw").notNull(),
+});
